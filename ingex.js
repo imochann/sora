b7663fdf2d55c85ffe6296b86359c7d61b2b68c7
Sharking9 <manafhadi101@gmail.com>
2023-08-20 21:09:30 +0900

    Company Table Pict & QA Feature Add

--- ingex.js
+++ ingex.js
@@ -1,5 +1,25 @@
-    // Saat halaman dimuat, pasang event listener untuk mengontrol visibilitas tombol
-    document.addEventListener('DOMContentLoaded', function() {
+// Q&A Funct
+const faqs = document.querySelectorAll('.faq');
+
+function toggleAnswer() {
+  const answer = this.nextElementSibling;
+  if (answer.style.maxHeight) {
+    answer.style.padding = '0px 10px 0px 100px'; // Menghapus padding saat elemen tertutup
+    answer.style.maxHeight = null;
+  } else {
+    answer.style.padding = '10px 10px 30px 100px'; // Menambahkan padding saat elemen terbuka
+    answer.style.maxHeight = answer.scrollHeight + 'px';
+  }
+}
+
+faqs.forEach(faq => {
+  const question = faq.querySelector('.question');
+  question.addEventListener('click', toggleAnswer);
+});
+
+// backToTopButton Function
+// Saat halaman dimuat, pasang event listener untuk mengontrol visibilitas tombol
+document.addEventListener('DOMContentLoaded', function() {
       var backToTopButton = document.getElementById('back-to-top');
       window.addEventListener('scroll', function() {
         if (window.scrollY > 100) {
@@ -14,3 +34,5 @@
         window.scrollTo({ top: 0, behavior: 'smooth' });
       });
     });
+
+
