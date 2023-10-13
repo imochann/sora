0b30f87ae887de8ed33d75e22eedb996861763dd
Sharking9 <manafhadi101@gmail.com>
2023-10-14 06:26:55 +0900

    Add Nav Arrow Slide Show Ver 15

--- JS/Main_code.js
+++ JS/Main_code.js
@@ -25,9 +25,13 @@ function toggleSlidviewer() {
   if (slidviewer.style.maxHeight) {
     slidviewer.style.padding = '0px 10px 0px 20px'; // Menghapus padding saat elemen tertutup
     slidviewer.style.maxHeight = null;
+    document.getElementById('slidbtntext').textContent = "詳しくはこちら"; // Perbarui teks button
+    document.getElementById('slidarrow').textContent = "▼"; // Perbarui teks button dengan karakter Unicode
   } else {
     slidviewer.style.padding = '10px 10px 30px 20px'; // Menambahkan padding saat elemen terbuka
     slidviewer.style.maxHeight = slidviewer.scrollHeight + 'px';
+    document.getElementById('slidbtntext').textContent = "閉じる ❌"; // Perbarui teks button
+    document.getElementById('slidarrow').textContent = ""; // Perbarui teks button dengan karakter Unicode
   }
 }
 
@@ -39,7 +43,7 @@ slides1.forEach(slides => {
 
 ////////Slider Nav Func////////////////
 $(document).ready(function () {
-  var currentSlider = 1;
+  var currentSlider = 2;
 
   // Inisialisasi semua slider saat halaman dimuat
   $('.slider').slick({
@@ -52,7 +56,7 @@ $(document).ready(function () {
   });
 
   // Sembunyikan semua slider kecuali yang pertama saat halaman dimuat
-  $('.slider:not(.slider1)').hide();
+  $('.slider:not(.slider2)').hide();
 
   // Fungsi untuk menampilkan slider yang dipilih
   function showSlider(sliderNumber) {
@@ -60,8 +64,8 @@ $(document).ready(function () {
       $('.slider' + sliderNumber).show();
       currentSlider = sliderNumber;
       $('#slider-number').text('令和５年 ' + currentSlider + '月'); // Perbarui teks slider-number
-      $('#prev-month').text(currentSlider - 1); // Perbarui teks　month
-      $('#next-month').text(currentSlider + 1); // Perbarui teks　month
+      $('#prev-month').text((currentSlider - 1) + "月"); // Perbarui teks　month
+      $('#next-month').text((currentSlider + 1) + "月"); // Perbarui teks　month
   }
 
   // Tampilkan slider pertama saat halaman dimuat
@@ -70,7 +74,7 @@ $(document).ready(function () {
   // Fungsi untuk navigasi ke slider selanjutnya
   $('.next-sliders').click(function () {
       currentSlider++;
-      if (currentSlider > 4) {
+      if (currentSlider > 6) {
           currentSlider = 1;
       }
       showSlider(currentSlider);
@@ -80,7 +84,7 @@ $(document).ready(function () {
   $('.prev-sliders').click(function () {
       currentSlider--;
       if (currentSlider < 1) {
-          currentSlider = 4;
+          currentSlider = 6;
       }
       showSlider(currentSlider);
   });
